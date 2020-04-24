6b91a99e27744a4553b44ca2709e7ab1e38792b3
Jamie Bliss <jamie@ivyleav.es>
2020-04-24 18:11:57 -0400

    Attempt to fix booting problems, failing.

--- main/time.c
+++ main/time.c
@@ -1,4 +1,3 @@
-#include "nvs_flash.h"
 #include "esp_event.h"
 #include "esp_log.h"
 #include "esp_sntp.h"
@@ -19,14 +18,15 @@ void initialize_sntp(void)
     sntp_setservername(0, "pool.ntp.org");
     sntp_set_time_sync_notification_cb(time_sync_notification_cb);
     sntp_init();
+
+
+    ESP_ERROR_CHECK(esp_netif_init());
+    ESP_ERROR_CHECK( esp_event_loop_create_default() );
+
 }
 
 void obtain_time(void)
 {
-    ESP_ERROR_CHECK( nvs_flash_init() );
-    ESP_ERROR_CHECK(esp_netif_init());
-    ESP_ERROR_CHECK( esp_event_loop_create_default() );
-
     /* This helper function configures Wi-Fi or Ethernet, as selected in menuconfig.
      * Read "Establishing Wi-Fi or Ethernet Connection" section in
      * examples/protocols/README.md for more information about this function.
